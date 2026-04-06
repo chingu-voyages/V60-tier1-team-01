d36517b5dc86543bb55c827bc51de3f70563b23d
Dustin Hoeppner <dhoepp@me.com>
2026-04-06 09:04:04 -0500

    feat: implement layout scaffolding with header, footer, and page routing

---
+++ src/pages/AddApplication/AddApplication.js
@@ -0,0 +1,5 @@
+import template from './AddApplication.html?raw';
+
+export function AddApplication() {
+  return template;
+}
---
+++ src/pages/Applications/Applications.js
@@ -0,0 +1,5 @@
+import template from './Applications.html?raw';
+
+export function Applications() {
+  return template;
+}
